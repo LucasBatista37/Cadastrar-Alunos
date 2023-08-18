c2d8e2865ad68a0afe7e17007c6cdfe502b8413a
Lucas Batista <124750301+LucasBatista37@users.noreply.github.com>
2023-08-17 23:02:47 -0300

    Add files via upload

--- array.php
+++ array.php
@@ -5,70 +5,67 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Array</title>
-    <style>
-        table{
-            border: 1px solid;
-        }
-        th{
-            border: 1px solid;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Cadastro de Alunos</h1>
 
+    <div id="dados">
     <form method="post">
+        <h2>Cadastrar Aluno</h2>
         <label for="nome">Nome</label>
         <input type="text" name="nome" id="nome">
-        <br><br>
         <label for="idade">Idade</label>
         <input type="number" name="idade" id="idade">
-        <br><br>
-        <label for="serie">Serie</label>
+        <label for="serie">Série</label>
         <input type="number" name="serie" id="serie">
-        <br><br>
-        <label for="media">Media</label>
+        <label for="media">Média</label>
         <input type="number" name="media" id="media">
         <br><br>
-        <input type="submit" value="Cadastrar">
-        <br><br>
+        <button>Cadastrar</button>
+        <br>
     </form>
-
-<table>
-    <thead>
-        <tr>
-            <h1>Resultado</h1>
-            <br>
-            <th>Nome</th>
-            <th>Idade</th>
-            <th>Serie</th>
-            <th>Média</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        
-    $nome = $_POST['nome'];
-    $idade = $_POST['idade'];
-    $serie = $_POST['serie'];
-    $media_final = $_POST['media'];
-        
-        $Alunos = array(
-            array($nome, $idade, $serie, $media_final),
-        );
-
-        foreach($Alunos as $linhas){
-
-            echo "<th>";
-
-                foreach($linhas as $valor){
-                    echo "<td> $valor </td>";
+    </div>
+    <div id="cad">
+    <h2>Alunos Cadastrados</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Idade</th>
+                <th>Série</th>
+                <th>Média</th>
+            </tr>
+        </thead>
+    </div>
+        <tbody>
+            <?php
+            session_start();
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $nome = $_POST['nome'];
+                $idade = $_POST['idade'];
+                $serie = $_POST['serie'];
+                $media_final = $_POST['media'];
+    
+                if (!isset($Alunos)) {
+                    $Alunos = array();
                 }
+    
+            $novoAluno = array($nome, $idade, $serie, $media_final);
+    
+            $_SESSION['Alunos'][] = $novoAluno;
 
-            echo "<th>";
-        }
-    ?>
-    </tbody>
-</table>
+            foreach ($_SESSION['Alunos'] as $linhas) {
+                echo "<tr>";
+
+                foreach ($linhas as $valor) {
+                    echo "<td>$valor</td>";
+            }
+                echo "</tr>";
+            }
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
